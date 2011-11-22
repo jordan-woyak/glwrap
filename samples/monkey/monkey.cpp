1c0783fd31ab76932c4068fd70cd6efc9098b702
Jordan Woyak <jordan.woyak@gmail.com>
2011-11-21 22:52:00 -0600

    working on monkey normal map

--- samples/monkey/monkey.cpp
+++ samples/monkey/monkey.cpp
@@ -26,8 +26,7 @@ int main()
 	};
 
 	// load textures
-	gl::texture_2d tex_color(glc);
-	gl::texture_2d tex_shininess(glc);
+	gl::texture_2d tex_color(glc), tex_spec(glc), tex_normal(glc);
 
 	//boost::gil::rgb8_image_t texdata;
 	//boost::gil::png_read_image("monkey.png", texdata);
@@ -36,8 +35,11 @@ int main()
 	if (texdata.LoadFromFile("../color.png"))
 		tex_color.assign(gl::unpack(texdata.GetPixelsPtr(), gl::pixel_format::rgba, {texdata.GetWidth(), texdata.GetHeight()}));
 
-	if (texdata.LoadFromFile("../shininess.png"))
-		tex_shininess.assign(gl::unpack(texdata.GetPixelsPtr(), gl::pixel_format::rgba, {texdata.GetWidth(), texdata.GetHeight()}));
+	if (texdata.LoadFromFile("../spec.png"))
+		tex_spec.assign(gl::unpack(texdata.GetPixelsPtr(), gl::pixel_format::rgba, {texdata.GetWidth(), texdata.GetHeight()}));
+
+	if (texdata.LoadFromFile("../normal.png"))
+		tex_normal.assign(gl::unpack(texdata.GetPixelsPtr(), gl::pixel_format::rgba, {texdata.GetWidth(), texdata.GetHeight()}));
 
 	// nonsense to load ply
 	ply::vertex_format<FooVertex> vert_fmt;
@@ -63,13 +65,15 @@ int main()
 
 	gl::texture_unit_alloter units(glc);
 	auto tex_color_unit = units.allot<gl::texture_2d>();
-	auto tex_shininess_unit = units.allot<gl::texture_2d>();
+	auto tex_spec_unit = units.allot<gl::texture_2d>();
+	auto tex_normal_unit = units.allot<gl::texture_2d>();
 
 	// create program
 	gl::program prog(glc);
 
 	auto tex_color_uni = prog.create_uniform<gl::texture_2d>("tex_color");
-	auto tex_shininess_uni = prog.create_uniform<gl::texture_2d>("tex_shininess");
+	auto tex_spec_uni = prog.create_uniform<gl::texture_2d>("tex_spec");
+	auto tex_normal_uni = prog.create_uniform<gl::texture_2d>("tex_normal");
 
 	auto modelview_uni = prog.create_uniform<gl::mat4>("modelview");
 	auto projection_uni = prog.create_uniform<gl::mat4>("projection");
@@ -77,7 +81,8 @@ int main()
 	auto light_dir_uni = prog.create_uniform<gl::vec3>("light_dir");
 	auto diff_color_uni = prog.create_uniform<gl::vec4>("diff_color");
 	auto spec_color_uni = prog.create_uniform<gl::vec4>("spec_color");
-	auto ambient_uni = prog.create_uniform<gl::float_t>("ambient");
+	auto ambient_uni = prog.create_uniform<gl::vec4>("ambient");
+	auto shininess_uni = prog.create_uniform<gl::float_t>("shininess");
 
 	auto pos_attrib = prog.create_attribute<gl::vec3>("pos");
 	auto norm_attrib = prog.create_attribute<gl::vec3>("norm");
@@ -87,27 +92,16 @@ int main()
 
 	prog.set_vertex_shader_source(
 		"out vec2 tpos;"
-		"out float spec_amt;"
-		"out float diffuse_amt;"
+		"out vec3 vertex_normal, norm_light_dir, E;"
 
 		"void main(void)"
 		"{"
 			"tpos = texpos;"
 
-			"vec3 norm_light_dir = normalize(light_dir);"
+			"norm_light_dir = normalize(light_dir);"
+			"vertex_normal = normalize(mat3(modelview) * norm);"
 
-			"vec3 vertex_normal = normalize(mat3(modelview) * norm);"
-			"diffuse_amt = max(dot(vertex_normal, norm_light_dir), 0.0);"
-
-			"vec3 eye = normalize(mat3(projection) * vec3(0, 0, -1));"
-			"spec_amt = dot(vertex_normal, normalize(eye + norm_light_dir));"
-
-			"float spec_cutoff = 0.9;"
-
-			"if (spec_amt < spec_cutoff)"
-				"spec_amt = 0.0;"
-			"else "
-				"spec_amt = mix(0.0, 1.0, (spec_amt - spec_cutoff) / (1 - spec_cutoff));"
+			"E = normalize(mat3(projection) * vec3(0, 0, -1));"
 
 			"gl_Position = modelview * projection * vec4(pos, 1);"
 		"}"
@@ -115,17 +109,24 @@ int main()
 
 	prog.set_fragment_shader_source(
 		"in vec2 tpos;"
-		"in float spec_amt;"
-		"in float diffuse_amt;"
+		"in vec3 vertex_normal, norm_light_dir, E;"
 
 		"void main(void)"
 		"{"
-			"vec4 base_color = texture2D(tex_color, tpos);"
+			"vec3 adjusted_normal = vertex_normal;"
+			//"vec3 adjusted_normal = normalize(mat3(modelview) *  texture2D(tex_normal, tpos).rgb);"
 
-			"vec3 spec = spec_amt * texture2D(tex_shininess, tpos).r * spec_color.rgb;"
+			"vec4 mat_color = texture2D(tex_color, tpos);"
 
-			"vec4 final_color = vec4("
-				"(ambient + diffuse_amt * diff_color.rgb) * base_color.rgb + spec, base_color.a);"
+			"vec3 Ia = ambient.rgb * mat_color.rgb * ambient.a;"
+
+			"float LambertTerm = max(dot(vertex_normal, norm_light_dir), 0.0);"
+			"vec3 Id = diff_color.rgb * mat_color.rgb * diff_color.a * LambertTerm;"
+
+			"vec3 R = reflect(-norm_light_dir, vertex_normal);"
+			"vec3 Is = spec_color.rgb * spec_color.a * pow(max(dot(R, E), 0.0), shininess);"
+
+			"vec4 final_color = vec4(Ia + Id + Is, mat_color.a);"
 
 			"fragdata = final_color;"
 		"}"
@@ -163,16 +164,19 @@ int main()
 
 	prog.set_uniform(diff_color_uni, {1, 1, 1, 1});
 	prog.set_uniform(spec_color_uni, {1, 1, 1, 0.1});
-	prog.set_uniform(ambient_uni, 0.2);
+	prog.set_uniform(ambient_uni, {1, 1, 1, 0.2});
 	prog.set_uniform(light_dir_uni, {0, 0.75, -1});
+	prog.set_uniform(shininess_uni, 50);
 
 	prog.set_uniform(projection_uni, {});
 
 	prog.set_uniform(tex_color_uni, tex_color_unit);
-	prog.set_uniform(tex_shininess_uni, tex_shininess_unit);
+	prog.set_uniform(tex_spec_uni, tex_spec_unit);
+	prog.set_uniform(tex_normal_uni, tex_normal_unit);
 
 	glc.bind_texture(tex_color_unit, tex_color);
-	glc.bind_texture(tex_shininess_unit, tex_shininess);
+	glc.bind_texture(tex_spec_unit, tex_spec);
+	glc.bind_texture(tex_normal_unit, tex_normal);
 
 	// TODO: kill
 	glc.bind_default_framebuffer();
