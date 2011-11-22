1026a68861eea0eb13786f8da130662ca5b441e8
Jordan Woyak <jordan.woyak@gmail.com>
2011-11-22 02:04:40 -0600

    torus sample

---
+++ samples/torus/torus.cpp
@@ -0,0 +1,162 @@
+
+#include <iostream>
+
+// lame
+#include "../bunny/ply.hpp"
+#include "glwrap/gl.hpp"
+
+int main()
+{
+	gl::ivec2 window_size{480, 480};
+
+	gl::context glc;
+	gl::display dsp(glc, window_size);
+	dsp.set_caption("glwrap-torus");
+
+	// custom vertex type
+	struct FooVertex
+	{
+		gl::vec3 pos, norm;
+	};
+
+	// nonsense to load ply
+	ply::vertex_format<FooVertex> vert_fmt;
+	vert_fmt.bind("x", [](std::istream& s, FooVertex& v){ s >> v.pos.x; });
+	vert_fmt.bind("y", [](std::istream& s, FooVertex& v){ s >> v.pos.y; });
+	vert_fmt.bind("z", [](std::istream& s, FooVertex& v){ s >> v.pos.z; });
+	vert_fmt.bind("nx", [](std::istream& s, FooVertex& v){ s >> v.norm.x; });
+	vert_fmt.bind("ny", [](std::istream& s, FooVertex& v){ s >> v.norm.y; });
+	vert_fmt.bind("nz", [](std::istream& s, FooVertex& v){ s >> v.norm.z; });
+
+	std::vector<FooVertex> vertices;
+	std::vector<gl::uint_t> indices;
+
+	ply::load("../torus.ply", vert_fmt, vertices, indices);
+
+	// allot generic vetex attrib locations
+	gl::attribute_location_alloter locs(glc);
+	auto pos_loc = locs.allot<gl::vec3>();
+	auto norm_loc = locs.allot<gl::vec3>();
+
+	// create program
+	gl::program prog(glc);
+
+	auto modelview_uni = prog.create_uniform<gl::mat4>("modelview");
+	auto projection_uni = prog.create_uniform<gl::mat4>("projection");
+
+	auto light_dir_uni = prog.create_uniform<gl::vec3>("light_dir");
+	auto diff_color_uni = prog.create_uniform<gl::vec4>("diff_color");
+	auto spec_color_uni = prog.create_uniform<gl::vec4>("spec_color");
+	auto mat_color_uni = prog.create_uniform<gl::vec4>("mat_color");
+	auto ambient_uni = prog.create_uniform<gl::vec4>("ambient");
+	auto shininess_uni = prog.create_uniform<gl::float_t>("shininess");
+
+	auto pos_attrib = prog.create_attribute<gl::vec3>("pos");
+	auto norm_attrib = prog.create_attribute<gl::vec3>("norm");
+
+	auto fragdata = prog.create_fragdata<gl::vec4>("fragdata");
+
+	prog.set_vertex_shader_source(
+		"out vec4 final_color;"
+
+		"void main(void)"
+		"{"
+			"vec3 norm_light_dir = normalize(light_dir);"
+			"vec3 vertex_normal = normalize(mat3(modelview) * norm);"
+			"vec3 E = normalize(mat3(projection) * vec3(0, 0, -1));"
+
+			"vec3 Ia = ambient.rgb * ambient.a;"
+
+			"float LambertTerm = max(dot(vertex_normal, norm_light_dir), 0.0);"
+			"vec3 Id = diff_color.rgb * diff_color.a * LambertTerm;"
+
+			"vec3 R = reflect(-norm_light_dir, vertex_normal);"
+			"vec3 Is = spec_color.rgb * spec_color.a * pow(max(dot(R, E), 0.0), shininess);"
+
+			"vec3 base = mat_color.rgb;"
+
+			"final_color = vec4((Ia + Id) * base + Is, mat_color.a);"
+
+			"gl_Position = modelview * projection * vec4(pos, 1);"
+		"}"
+	);
+
+	prog.set_fragment_shader_source(
+		"in vec4 final_color;"
+
+		"void main(void)"
+		"{"
+			"fragdata = final_color;"
+		"}"
+	);
+
+	prog.compile();
+
+	// bind attribute and fragdata location before linking
+	prog.bind_fragdata(fragdata, glc.draw_buffer(0));
+
+	prog.bind_attribute(pos_attrib, pos_loc);
+	prog.bind_attribute(norm_attrib, norm_loc);
+
+	prog.link();
+
+	//if (!prog.is_good())
+		std::cout << "program log:\n" << prog.get_log() << std::endl;
+
+	// load vertex data
+	gl::array_buffer<FooVertex> verbuf(glc);
+	verbuf.assign(vertices);
+
+	// load index data
+	gl::index_buffer<gl::uint_t> indbuf(glc);
+	indbuf.assign(indices);
+
+	// automatically set data types, sizes and strides to components of custom vertex type
+	gl::vertex_array arr(glc);
+	arr.bind_vertex_attribute(pos_loc, verbuf.attrib() | &FooVertex::pos);
+	arr.bind_vertex_attribute(norm_loc, verbuf.attrib() | &FooVertex::norm);
+
+	gl::float_t rotate = 0;
+
+	prog.set_uniform(diff_color_uni, {1, 1, 0.75, 1});
+	prog.set_uniform(spec_color_uni, {1, 1, 1, 0.1});
+	prog.set_uniform(ambient_uni, {1, 1, 1, 0.2});
+	prog.set_uniform(light_dir_uni, {0, 0, -1});
+	prog.set_uniform(shininess_uni, 5);
+
+	prog.set_uniform(mat_color_uni, {0, 0.5, 1, 1});
+
+	prog.set_uniform(projection_uni, {});
+
+	glc.enable(gl::capability::depth_test);
+	//glc.enable(gl::capability::cull_face);
+	//glc.front_face(gl::orientation::cw);
+
+	// TODO: kill
+	glc.bind_default_framebuffer();
+
+	dsp.set_display_func([&]
+	{
+		glc.clear_color({0.2, 0.2, 0.2, 1});
+		glc.clear_depth(1.0);
+
+		// rotating projection
+		gl::mat4 modelview = gl::rotate(rotate, 0, 1, 0) * gl::rotate(rotate * 2, 1, 0, 0) *
+			gl::translate(0, 0, -4) *
+			gl::perspective(45, (float_t)window_size.x / window_size.y, 1, 100);
+
+		prog.set_uniform(modelview_uni, modelview);
+
+		if ((rotate += 3.14 / 360) >= 3.14 * 2)
+			rotate -= 3.14 * 2;
+
+		glc.draw_elements(prog, gl::primitive::triangles, arr, indbuf, 0, indices.size());
+	});
+
+	dsp.set_resize_func([&](gl::ivec2 const& _size)
+	{
+		glc.viewport({0, 0}, window_size = _size);
+	});
+
+	dsp.run_loop();
+}
