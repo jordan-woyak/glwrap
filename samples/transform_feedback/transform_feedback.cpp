e2a849ed78227f7a93bf21209608ae60a9015e78
Jordan Woyak <jordan.woyak@gmail.com>
2011-11-28 17:46:44 -0600

    better transform feedback sample code

--- samples/transform_feedback/transform_feedback.cpp
+++ samples/transform_feedback/transform_feedback.cpp
@@ -10,27 +10,29 @@ int main()
 	// used to connect array_buffer vertex data and program attributes together
 	// via the typeless "generic vertex attributes" in a type-safe manner
 	gl::attribute_location_alloter locs(glc);
-	auto input1_loc = locs.allot<gl::vec3>();
-	auto input2_loc = locs.allot<gl::vec3>();
+	auto input1_loc = locs.allot<gl::int_t>();
+	auto input2_loc = locs.allot<gl::int_t>();
 
 	// connect buffers to varyings in a type-safe manner
 	gl::transform_feedback_binding_alloter tfbs(glc);
-	auto feedback_out = tfbs.allot<gl::vec3>();
+	auto feedback_out = tfbs.allot<gl::float_t>();
 
 	// create a program
 	gl::program prog(glc);
 
 	// define some variables in the program,
 	// they are automatically added to the program source
-	auto input1_attrib = prog.create_attribute<gl::vec3>("input1");
-	auto input2_attrib = prog.create_attribute<gl::vec3>("input2");
+	auto input1_attrib = prog.create_attribute<gl::int_t>("input1");
+	auto input2_attrib = prog.create_attribute<gl::int_t>("input2");
 
-	auto output1_varying = prog.create_vertex_out_varying<gl::vec3>("output1");
+	auto output1_varying = prog.create_vertex_out_varying<gl::float_t>("output1");
+
+	auto operand1_uni = prog.create_uniform<gl::float_t>("operand1");
 
 	prog.set_vertex_shader_source(
 		"void main(void)"
 		"{"
-			"output1 = cross(input1, input2);"
+			"output1 = input1 * input2 + operand1;"
 		"}"
 	);
 
@@ -48,47 +50,56 @@ int main()
 		std::cout << "program log:\n" << prog.get_log() << std::endl;
 
 	// custom vertex type
-	struct FooVertex
+	struct Input
 	{
-		gl::vec3 input1;
-		gl::vec3 input2;
+		gl::int_t input1, input2;
 	};
 
 	// input buffer
-	gl::buffer<FooVertex> input_buffer(glc);
-	input_buffer.assign((FooVertex[])
+	gl::buffer<Input> input_buffer(glc);
+	input_buffer.assign((Input[])
 	{
-		{{-1, 0, 1}, {1, 0, 0}},
-		{{1, 0, 0}, {0, 1, 0}},
-		{{0, -1, 0}, {0, 0, 1}},
-		{{0, 0, 1}, {0, -1, 0}},
+		{45, 1}, {2, 3}, {7, 7}, {23, 2}, {42, 7}, {1, 499}, {89, 3}, {12, 7}, {3, 3}
 	});
 
 	// set buffers, strides, data types, etc
 	gl::vertex_array input_vertices(glc);
-	input_vertices.bind_vertex_attribute(input1_loc, input_buffer.begin() | &FooVertex::input1);
-	input_vertices.bind_vertex_attribute(input2_loc, input_buffer.begin() | &FooVertex::input2);
+	input_vertices.bind_vertex_attribute(input1_loc, input_buffer.begin() | &Input::input1);
+	input_vertices.bind_vertex_attribute(input2_loc, input_buffer.begin() | &Input::input2);
+
+	std::vector<gl::float_t> operands =
+	{
+		1000, 2000, 7, 500, -1000
+	};
 
 	// output buffer
-	gl::buffer<gl::vec3> output_buffer(glc);
-	output_buffer.storage(input_buffer.size());
+	gl::buffer<gl::float_t> output_buffer(glc);
+	output_buffer.storage(input_buffer.size() * operands.size());
 
 	glc.bind_buffer(feedback_out, output_buffer.begin(), output_buffer.size());
 
-	// TRANSFORM FEEEEDBAAACK!
 	glc.use_program(prog);
 	glc.use_vertex_array(input_vertices);
 	glc.use_primitive_mode(gl::primitive::points);
 
+	// transform feedback
 	glc.start_transform_feedback(gl::primitive::points);
-	glc.draw_arrays(0, input_buffer.size());
+
+	for (auto operand : operands)
+	{
+		prog.set_uniform(operand1_uni, operand);
+		glc.draw_arrays(0, input_buffer.size());
+	}
+
 	glc.stop_transform_feedback();
 
 	glc.finish();
 	std::cout << "DONE" << std::endl << std::endl;
 
 	// print results
-	gl::mapped_buffer<gl::vec3> output_view(output_buffer);
-	for (auto& vert : output_view)
-		std::cout << boost::format("{%d, %d, %d}\n") % vert.x % vert.y % vert.z;
+	std::size_t i{};
+	for (auto& vert : gl::mapped_buffer<gl::float_t>(output_buffer))
+		std::cout << vert << "\t" << std::string(0 == (++i % input_buffer.size()), '\n');
+
+	std::cout << std::endl;
 }
