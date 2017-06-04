99512d65fd6468bcc9db562875af614f2127ace8
Jordan Woyak <jordan.woyak@gmail.com>
2017-06-04 10:30:23 -0500

    Move from stuff from util to traits.

--- include/glwrap/util.hpp
+++ include/glwrap/util.hpp
@@ -2,8 +2,6 @@
 #pragma once
 
 #include <cstdint>
-#include <vector>
-#include <list>
 
 #include <chrono>
 
@@ -11,6 +9,7 @@
 
 #include "vector.hpp"
 #include "exception.hpp"
+#include "detail/traits.hpp"
 
 namespace GLWRAP_NAMESPACE
 {
@@ -176,167 +175,6 @@ template <typename R, typename... Args>
 gl_function_caller<R, Args...> make_gl_function_caller(R(*_func)(Args...), const char* _name)
 {
 	return {_func, _name};
-}
-
-template <typename T, typename Enable = void>
-struct is_gl_integral : std::false_type {};
-
-template <typename T>
-struct is_gl_integral<T, typename std::enable_if<
-		std::is_same<gl::byte_t, T>::value ||
-		std::is_same<gl::ubyte_t, T>::value ||
-		std::is_same<gl::short_t, T>::value ||
-		std::is_same<gl::ushort_t, T>::value ||
-		std::is_same<gl::int_t, T>::value ||
-		std::is_same<gl::uint_t, T>::value>::type> : std::true_type
-{};
-
-template <typename T, typename Enable = void>
-struct is_gl_floating_point : std::false_type
-{};
-
-// TODO: half_t
-template <typename T>
-struct is_gl_floating_point<T, typename std::enable_if<
-		//std::is_same<gl::half_t, T>::value ||
-		std::is_same<gl::float_t, T>::value ||
-		std::is_same<gl::double_t, T>::value>::type> : std::true_type
-{};
-
-template <typename T>
-constexpr GLenum data_type_enum()
-{
-	static_assert(false && std::is_void<T>::value, "Invalid data type.");
-	return 0;
-}
-
-template <>
-constexpr inline GLenum data_type_enum<ubyte_t>()
-{
-	return GL_UNSIGNED_BYTE;
-}
-
-template <>
-constexpr inline GLenum data_type_enum<byte_t>()
-{
-	return GL_BYTE;
-}
-
-template <>
-constexpr inline GLenum data_type_enum<ushort_t>()
-{
-	return GL_UNSIGNED_SHORT;
-}
-
-template <>
-constexpr inline GLenum data_type_enum<short_t>()
-{
-	return GL_SHORT;
-}
-
-template <>
-constexpr inline GLenum data_type_enum<uint_t>()
-{
-	return GL_UNSIGNED_INT;
-}
-
-template <>
-constexpr inline GLenum data_type_enum<int_t>()
-{
-	return GL_INT;
-}
-/*
-template <>
-constexpr inline GLenum data_type_enum<half_t>()
-{
-	return GL_HALF_FLOAT;
-}
-*/
-template <>
-constexpr inline GLenum data_type_enum<float_t>()
-{
-	return GL_FLOAT;
-}
-
-template <>
-constexpr inline GLenum data_type_enum<double_t>()
-{
-	return GL_DOUBLE;
-}
-
-// C++17's void_t
-template <class...>
-using void_t = void;
-
-template <typename T>
-struct remove_cvref : std::remove_cv<typename std::remove_reference<T>::type> {};
-
-template <typename T>
-struct is_std_vector : std::false_type {};
-
-template <typename... T>
-struct is_std_vector<std::vector<T...>> : std::true_type {};
-
-template <typename T>
-struct is_std_vector_of_bool : std::false_type {};
-
-template <typename A>
-struct is_std_vector_of_bool<std::vector<bool, A>> : std::true_type {};
-
-template <typename T>
-struct is_std_array : std::false_type {};
-
-template <typename T, std::size_t N>
-struct is_std_array<std::array<T, N>> : std::true_type {};
-
-template <typename T>
-struct is_std_initializer_list : std::false_type {};
-
-template <typename T>
-struct is_std_initializer_list<std::initializer_list<T>> : std::true_type {};
-
-template<typename T, typename Enable = void>
-struct is_range : std::false_type {};
-
-template<typename T>
-struct is_range<T, void_t<
-		//typename T::value_type,
-		//typename T::size_type,
-		//typename T::allocator_type,
-		//typename T::iterator,
-		//typename T::const_iterator,
-		decltype(std::begin(std::declval<T>())),
-		decltype(std::end(std::declval<T>()))
-		//decltype(std::declval<T>().end()),
-		//decltype(std::declval<T>().cbegin()),
-		//decltype(std::declval<T>().cend())
-			>> : std::true_type {};
-
-// TODO: initializer list?
-
-template <typename T>
-struct is_contiguous
-{
-	typedef typename remove_cvref<T>::type adj_type;
-
-	static const bool value =
-		(is_std_vector<adj_type>::value && !is_std_vector_of_bool<adj_type>::value)
-		|| is_std_array<adj_type>::value
-		|| std::is_array<adj_type>::value;
-};
-
-static_assert(is_contiguous<std::vector<int>&>::value, "fail");
-static_assert(is_contiguous<const std::array<float, 4>&>::value, "fail");
-static_assert(is_contiguous<int[5]>::value, "fail");
-static_assert(is_contiguous<float(&)[1]>::value, "fail");
-static_assert(!is_contiguous<std::list<char>>::value, "fail");
-static_assert(!is_contiguous<std::vector<bool>>::value, "fail");
-
-template <typename T, typename M>
-constexpr std::intptr_t get_member_offset(M T::*_member)
-{
-	// TODO: not portable
-	return reinterpret_cast<std::intptr_t>(&(static_cast<const T*>(nullptr)->*_member));
 }
 
 template <typename LFunc, typename GFunc>
